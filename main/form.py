de369726f98e45ef321cad02f7387b52174f46ae
sethupathi75 <83010699+sethupathi75@users.noreply.github.com>
2021-09-07 18:05:31 +0530

    Delete main directory

--- main/form.py
+++
@@ -1,7 +0,0 @@
-from  django import forms
-from computerstore.models import computerstore
-
-class store(forms.ModelForm):
-    class Meta:
-        model=computerstore
-        fields='__all__'
--- main/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class computerstore(models.Model):
-    Product_name=models.CharField(max_length=20)
-    Description=models.CharField(max_length=5000)
-    image=models.FileField(upload_to='images/')
